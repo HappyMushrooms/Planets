df59f843b295b13b7f07609d6996983916f130b7
grudjko <grudjko@gmail.com>
2017-11-04 13:00:22 +0300

    Finished stright test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -22,7 +22,7 @@ namespace ConsoleApp1
 
         private static void RunTests()
         {
-            ITest test=null;
+            ITest test=new StraightMotionTest();
             IMethod method = null;
             const double dt=0.1;
             State state = test.Generalinitialstate();
@@ -35,7 +35,7 @@ namespace ConsoleApp1
                     Console.WriteLine("Test failed");
                     break;
                 }
-                state = method.Calculate(state, dt);
+                //state = method.Calculate(state, dt);
             }
         }
     }
